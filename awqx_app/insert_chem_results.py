db7721d9a42954431605122fcc629c1b70cdf354
marybecker <mary.becker@ct.gov>
2021-07-28 17:09:28 -0400

    working on chem results insert

--- awqx_app/insert_chem_results.py
+++ awqx_app/insert_chem_results.py
@@ -7,16 +7,17 @@ import argparse
 des = """
 ------------------------------------------------------------------------------------------
 Import Chem Results into Ambient Water Data Exchange (awx) water quality database  
-Mary Becker - Last Updated 2021-03-10
+Mary Becker - Last Updated 2021-07-28
 ------------------------------------------------------------------------------------------
-Given input directory of excel template spreadsheets with new station information,
+Given input directory of spreadsheets with new lab results,
 automatically checks for constraints with the database schema and produces an
-error report for tuples that do not meet the constraints.  Tuples that do meet 
+error report for tuples that do not meet the constraints.  Tuples that meet 
 requirements are inserted into the Chem Results table """
 
 parser = argparse.ArgumentParser(description=des.lstrip(" "), formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-i', '--in_dir', type=str, help='input directory of ftp\t[None]')
 parser.add_argument('-c', '--cf_dir', type=str, help='input directory of config file')
+parser.add_argument('-d', '--db_scm', type=str, help='input database schema name')
 args = parser.parse_args()
 
 # build args into params...
@@ -27,6 +28,11 @@ else:
 
 if args.cf_dir is not None:
     cf_dir = args.cf_dir
+else:
+    raise IOError
+
+if args.db_scm is not None:
+    db_scm = args.db_scm
 else:
     raise IOError
 
@@ -63,18 +69,18 @@ headerList = ['ActivityIdentifier',	'lab_accession', 'characteristic_name',	'val
               'analysis_start_date', 'Rslt_status', 'comment', 'methodSpeciationName', 'result_detection_condition',
               'sample_fraction', 'method_number', 'method_context', 'DetectionQuantitationTypeName',
               'MDL', 'DetectionQuantitationTypeNameUOM']
-SQLinsert = 'INSERT INTO awqx_test.resultschem (`ActivityIdentifier`,`LabAccession`,`CharacteristicName`,' \
+SQLinsert = 'INSERT INTO ' + db_scm + '.resultschem (`ActivityIdentifier`,`LabAccession`,`CharacteristicName`,' \
             '`ResultMeasureValue`,`ResultMeasureUnitCode`,`ResultValueTypeName`,`AnalysisStartDate`,' \
             '`ResultStatusIdentifier`,`ResultComment`,`MethodSpeciationName`,`ResultDetectionConditionText`,' \
             '`ResultSampleFractionText`,`ResultAnalyticalMethodIdentifier`,' '`ResultAnalyticalMethodContext`,' \
             '`DetectionQuantitationTypeName`,`DetectionQuantitationLimitMeasureValue`,' \
             '`DetectionLimitMeasureUnitCode`,`createUser`,`createDate`,`lastUpdateUser`,`lastUpdateDate`) ' \
             'VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);'
-SQLselect = 'SELECT Name FROM awqx_test.methodspeciation;'
+SQLselect = 'SELECT Name FROM ' + db_scm + '.methodspeciation;'
 
-SQLerrLog = 'INSERT INTO awqx_test.errlog VALUES (?,?,?,?,?,?);'
+SQLerrLog = 'INSERT INTO ' + db_scm + '.errlog VALUES (?,?,?,?,?,?);'
 
-with msc.MYSQL('localhost', 'awqx_test', 3306, config_uid, config_pw) as dbo:
+with msc.MYSQL('localhost', db_scm, 3306, config_uid, config_pw) as dbo:
     select = dbo.query(SQLselect)
     print('found %s files to process: %s' % (len(fdir), fdir))
 
@@ -94,7 +100,7 @@ try:
         os.rename(fpathIn, fpathOut)
 
         if raw is not None and header == headerList:
-            with msc.MYSQL('localhost', 'awqx_test', 3306, config_uid, config_pw) as dbo:
+            with msc.MYSQL('localhost', db_scm, 3306, config_uid, config_pw) as dbo:
                 insDate = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
                 # Insert into the database line by line.  Append DB error if not caught by qc checks.
